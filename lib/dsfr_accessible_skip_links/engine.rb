bff1ed8de30cd9d9f994efe0d0bb2a7b0bba203d
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2025-08-27 16:22:21 +0200

    modification du fichier engine pour s'assurer que les vues de la gem sont accessibles

--- lib/dsfr_accessible_skip_links/engine.rb
+++ lib/dsfr_accessible_skip_links/engine.rb
@@ -4,6 +4,16 @@ module DsfrAccessibleSkipLinks
   class Engine < ::Rails::Engine
     isolate_namespace DsfrAccessibleSkipLinks
 
+    initializer "dsfr_accessible_skip_links.view_paths" do
+	    ActiveSupport.on_load(:action_controller) do
+		    prepend_view_path Engine.root.join("app", "views")
+	    end
+
+	    ActiveSupport.on_load(:action_mailer) do
+		    prepend_view_path Engine.root.join("app", "views")
+	    end
+    end
+
     config.to_prepare do
       ApplicationController.include(DsfrAccessibleSkipLinks::SkipLinks) if defined?(ApplicationController)
     end
